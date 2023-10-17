1f84575dca6ed896272954be00a88c5e3a674761
NathanosG <nate.turnbull9@gmail.com>
2023-10-17 14:54:40 +0100

    changes

---
+++ Taxi/Assets/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float balance;
+    public float crimeLevel;
+
+    public GameObject currentCar;
+    public GameObject jobs;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
